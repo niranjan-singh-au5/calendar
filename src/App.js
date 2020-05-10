01075ed8d93099234da4d3870c2a809e418b56cd
niranjan-singh-au5 <56333413+niranjan-singh-au5@users.noreply.github.com>
2020-05-11 02:03:31 +0530

    update

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ class App extends Component {
     if(this.props.reload){
       this.componentDidMount()
     }
-    if(this.props.error!==prevProps.error && this.props.error===false){
+    if(this.props.error!==prevProps.error && this.props.error){
       alert("Task has already been assigned in this time slot")
     }
     
